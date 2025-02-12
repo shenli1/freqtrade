8a70da27d791d4fe82ed27ad954176096473a48a
shenli <sl3697440@163.com>
2025-02-12 17:13:45 +0800

    新增E0V1E策略

--- start_freqtrade.sh
+++ start_freqtrade.sh
@@ -7,7 +7,7 @@ LOGFILE="freqtrade.log"
 PIDFILE="freqtrade.pid"
 
 # 启动 freqtrade 的 trade 模式，注意 nohup 会使进程在后台持续运行
-nohup freqtrade trade -c ./user_data/config.json --strategy HarmonicDivergence > "$LOGFILE" 2>&1 &
+nohup freqtrade trade -c ./user_data/config_dry.json --strategy E0V1E > "$LOGFILE" 2>&1 &
 
 # 将新启动进程的 PID 保存到文件中
 echo $! > "$PIDFILE"
